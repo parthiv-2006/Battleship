0c7cb7c26fec83e6f6f6230a9717598fa22ecf3d
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-30 09:34:46 -0400

    Add tests for placeShip method in gameboard class

--- src/index.test.js
+++ src/index.test.js
@@ -30,3 +30,45 @@ test('Creates a gameboard with 10 entries per row', () => {
     expect(element.length).toBe(10)
   })
 })
+
+describe('placeShip', () => {
+  let gameboard;
+  let ship;
+  
+  beforeEach(() => {
+    gameboard = new Gameboard();
+    ship = new Ship(3);
+  });
+
+  test('Ship is not placed with invalid coordinates', () => {
+    expect(gameboard.placeShip(ship, 10, 10)).toBe(false);
+  });
+
+  test('Ship is placed with valid coordinates (horizontal)', () => {
+    gameboard.placeShip(ship, 0, 0, true);
+    expect(gameboard.board[0][0]).toBe(ship);
+    expect(gameboard.board[0][1]).toBe(ship);
+    expect(gameboard.board[0][2]).toBe(ship);
+  });
+
+  test('Ship is placed with valid coordinates (vertical)', () => {
+    gameboard.placeShip(ship, 0, 0, false);
+    expect(gameboard.board[0][0]).toBe(ship);
+    expect(gameboard.board[1][0]).toBe(ship);
+    expect(gameboard.board[2][0]).toBe(ship);
+  });
+
+  test('does not place a ship on top of another ship (horizontal)', () => {
+    const ship2 = new Ship(3)
+    gameboard.placeShip(ship2, 0, 0, true);
+    expect(gameboard.placeShip(ship, 0, 0, true)).toBe(false);
+  })
+
+  test('does not place a ship on top of another ship (vertical)', () => {
+    const ship2 = new Ship(3);
+    gameboard.placeShip(ship2, 0, 0, false);
+    expect(gameboard.placeShip(ship, 0, 0, false)).toBe(false);
+  });
+
+})
+
