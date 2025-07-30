01e9c3a23972cba5b1123d5f63ed82aae2cf1465
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-30 10:10:15 -0400

    Add tests for allShipsSunk method in the gameboard class

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ import {Ship, Gameboard} from './index.js';
 
 test('Creates a ship object', () => {
   const myShip = new Ship(6)
-  expect(myShip).toEqual({length: 6, numHit: 0, sunk: false})
+  expect(myShip).toEqual({length: 6, numHit: 0})
 });
 
 test('Ship is hit once', () => {
@@ -16,7 +16,7 @@ test('Ship is sunk', () => {
   const myShip = new Ship(2)
   myShip.hit()
   myShip.hit()
-  expect(myShip.sunk).toBe(true)
+  expect(myShip.isSunk()).toBe(true)
 })
 
 test('Creates a gameboard with 10 rows', () => {
@@ -70,5 +70,57 @@ describe('placeShip', () => {
     expect(gameboard.placeShip(ship, 0, 0, false)).toBe(false);
   });
 
+  describe('receiveAttack', () => {
+    let gameboard;
+    let ship;
+
+    beforeEach(() => {
+      gameboard = new Gameboard();
+      ship = new Ship(3);
+      gameboard.placeShip(ship, 0, 0, true);
+    })
+
+    test('ship is hit', () => {
+      expect(gameboard.receiveAttack(0, 0)).toBe(true)
+    })
+
+    test('ship is not hit', () => {
+      expect(gameboard.receiveAttack(0, 1)).toBe(false)
+    })
+
+    test('ship hit counter is incremented', () => {
+      gameboard.receiveAttack(0, 0)
+      gameboard.receiveAttack(0, 1)
+      gameboard.receiveAttack(1, 0)
+      expect(ship.numHit).toBe(2)
+    })
+
+    test('records a missed attack on the board', () => {
+      gameboard.receiveAttack(5, 5)
+      expect(gameboard.board[5][5]).toBe('miss')
+    })
+  })
+
 })
+
+test('reports all ships sunk', () => {
+  const gameboard = new Gameboard()
+  const ship = new Ship(1)
+  const ship2 = new Ship(1)
+  gameboard.placeShip(ship2, 0, 1, true)
+  gameboard.placeShip(ship, 0, 0, true)
+  ship.hit()
+  ship2.hit()
+  expect(gameboard.allShipsSunk()).toBe(true)
+})
+
+test('reports when all ships are not sunk', () => {
+  const gameboard = new Gameboard();
+  const ship = new Ship(1);
+  const ship2 = new Ship(1);
+  gameboard.placeShip(ship2, 0, 1, true);
+  gameboard.placeShip(ship, 0, 0, true);
+  ship.hit();
+  expect(gameboard.allShipsSunk()).toBe(false);
+});
 
